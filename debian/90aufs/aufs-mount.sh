e064ac60dff8b2487fec0da209561574b763db7a
Thomas Lange <lange@informatik.uni-koeln.de>
2015-08-12 15:14:12 +0200

    use --bind and umount instead of --move, add comments Thanks to Moritz 'Morty' Strübe <morty@gmx.net> for the patch

--- debian/90aufs/aufs-mount.sh
+++ debian/90aufs/aufs-mount.sh
@@ -17,15 +17,24 @@ modprobe aufs
 # a little bit tuning
 mount -o remount,nolock,noatime $NEWROOT
 
+# Move root
+# NB: --move does not always work. Google >mount move "wrong fs"< for
+#     details
 mkdir -p /live/image
-mount --move $NEWROOT /live/image
+mount --bind $NEWROOT /live/image
+umount $NEWROOT
 
+# Create tmpfs
 mkdir /cow
 mount -n -t tmpfs -o mode=0755 tmpfs /cow
 
+# Merge both to new Filesystem
 mount -t aufs -o noatime,noxino,dirs=/cow=rw:/live/image=rr aufs $NEWROOT
 
+# Let filesystems survive pivot
 mkdir -p $NEWROOT/live/cow
 mkdir -p $NEWROOT/live/image
-mount --move /cow $NEWROOT/live/cow
-mount --move /live/image $NEWROOT/live/image
+mount --bind /cow $NEWROOT/live/cow
+umount /cow
+mount --bind /live/image $NEWROOT/live/image
+umount /live/image
