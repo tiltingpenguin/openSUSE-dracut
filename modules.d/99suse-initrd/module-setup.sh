tiltingpenguin/openSUSE-dracut

commit d393f006fa8d991465f879e78371ab8864d86058

fix(suse-initrd): exclude modules that are built-in
When checking the required modules a certain module needs, we need
to check whether those are already built-in or not.
If they are, we exclude them from the list of modules to install.

This is necessary as otherwise dracut-install fails when stumbling
over modules thar are already built-in.

Signed-off-by: Oscar Salvador <osalvador@suse.de>

modules.d/99suse-initrd/module-setup.sh
@@ -22,6 +22,7 @@ get_modprobe_conf_files() {
 # called by dracut
 installkernel() {
     local line mod reqs all_mods=
+    local BUILT_IN_PATH="/lib/modules/$(uname -r)/modules.builtin"
 
     while read -r line; do
         mod="${line##*SUSE INITRD: }"
@@ -29,7 +30,14 @@ installkernel() {
         reqs="${line##*REQUIRES }"
         if [[ ! $hostonly ]] || grep -q "^$mod\$" "$DRACUT_KERNEL_MODALIASES"
         then
-            all_mods="$all_mods $reqs"
+            for module in $reqs
+            do
+                if ! grep -q "/$module.ko" $BUILT_IN_PATH
+                then
+                    # The module is not built-in, so we can safely add it
+                    all_mods="$all_mods $module"
+                fi
+            done
         fi
     done <<< "$(grep -h "^# SUSE INITRD: " $(get_modprobe_conf_files))"
 

commit 62c3a764b4e6c3ca51dd052c2808b4ef5bfdf80d

fix(suse-initrd): use $kernel rather than $(uname -r)
$(uname -r) tests for the running kernel, while we may be building the
initrd for some other kernel.

Fixes: d393f00 ("fix(suse-initrd): exclude modules that are built-in")

modules.d/99suse-initrd/module-setup.sh
@@ -23,7 +23,7 @@ get_modprobe_conf_files() {
 # called by dracut
 installkernel() {
     local line mod reqs all_mods=
-    local BUILT_IN_PATH="/lib/modules/$(uname -r)/modules.builtin"
+    local BUILT_IN_PATH="/lib/modules/$kernel/modules.builtin"
 
     while read -r line; do
         mod="${line##*SUSE INITRD: }"