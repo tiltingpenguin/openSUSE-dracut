dracutdevs/dracut

commit 4257798f8ad8570e6918ca9b819ffa254d4316ff

fips: forward port RHEL-6 fips changes
- also support FIPS on separate LVM partition
- use small settle loop to get /boot
- "set -e" has no effect, if we use "||"
- make fips work with encrypted root and seperate boot
- moved to pre-pivot to support /boot in /

modules.d/01fips/fips.sh
@@ -2,11 +2,12 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-do_fipskernel()
+mount_boot()
 {
     boot=$(getarg boot=)
-    KERNEL=$(uname -r)
-    case "$boot" in
+
+    if [ -n "$boot" ]; then
+        case "$boot" in
         LABEL=*)
             boot="$(echo $boot | sed 's,/,\\x2f,g')"
             boot="/dev/disk/by-label/${boot#LABEL=}"
@@ -18,45 +19,52 @@ do_fipskernel()
             ;;
         *)
             die "You have to specify boot=<boot device> as a boot option for fips=1" ;;
-    esac
+        esac
 
-    if ! [ -e "$boot" ]; then
-        udevadm trigger --action=add >/dev/null 2>&1
-        [ -z "$UDEVVERSION" ] && UDEVVERSION=$(udevadm --version)
-
-        if [ $UDEVVERSION -ge 143 ]; then
-            udevadm settle --exit-if-exists=$boot
-        else
-            udevadm settle --timeout=30
+        if ! [ -e "$boot" ]; then
+            udevadm trigger --action=add >/dev/null 2>&1
+            [ -z "$UDEVVERSION" ] && UDEVVERSION=$(udevadm --version)
+            i=0
+            while ! [ -e $boot ]; do
+                if [ $UDEVVERSION -ge 143 ]; then
+                    udevadm settle --exit-if-exists=$boot
+                else
+                    udevadm settle --timeout=30
+                fi
+                [ -e $boot ] && break
+                modprobe scsi_wait_scan && rmmod scsi_wait_scan
+                [ -e $boot ] && break
+                sleep 0.5
+                i=$(($i+1))
+                [ $i -gt 40 ] && break
+            done
         fi
+
+        [ -e "$boot" ] || return 1
+
+        mkdir /boot
+        info "Mounting $boot as /boot"
+        mount -oro "$boot" /boot || return 1
     fi
-
-    [ -e "$boot" ]
-
-    mkdir -m 0755 /boot
-    info "Mounting $boot as /boot"
-    mount -oro "$boot" /boot
-
-    info "Checking integrity of kernel"
-
-    if ! [ -e "/boot/.vmlinuz-${KERNEL}.hmac" ]; then
-        warn "/boot/.vmlinuz-${KERNEL}.hmac does not exist"
-        return 1
-    fi
-
-    sha512hmac -c "/boot/.vmlinuz-${KERNEL}.hmac" || return 1
-
-    info "Umounting /boot"
-    umount /boot
 }
 
 do_fips()
 {
+    info "Checking integrity of kernel"
+    newroot=$NEWROOT
+    KERNEL=$(uname -r)
+
+    [ -e "$newroot/boot/.vmlinuz-${KERNEL}.hmac" ] || unset newroot
+
+    if ! [ -e "$newroot/boot/.vmlinuz-${KERNEL}.hmac" ]; then
+        warn "$newroot/boot/.vmlinuz-${KERNEL}.hmac does not exist"
+        return 1
+    fi
+
+    sha512hmac -c "$newroot/boot/.vmlinuz-${KERNEL}.hmac" || return 1
+
     FIPSMODULES=$(cat /etc/fipsmodules)
 
-    if ! getarg rd.fips.skipkernel >/dev/null; then
-	do_fipskernel
-    fi
     info "Loading and integrity checking all crypto modules"
     for module in $FIPSMODULES; do
         if [ "$module" != "tcrypt" ]; then
@@ -66,15 +74,11 @@ do_fips()
     info "Self testing crypto algorithms"
     modprobe tcrypt || return 1
     rmmod tcrypt
-    info "All initrd crypto checks done"  
+    info "All initrd crypto checks done"
+
+    > /tmp/fipsdone
+
+    umount /boot >/dev/null 2>&1
 
     return 0
 }
-
-if ! fipsmode=$(getarg fips) || [ $fipsmode = "0" ]; then
-    rm -f /etc/modprobe.d/fips.conf >/dev/null 2>&1
-else
-    set -e
-    do_fips || die "FIPS integrity test failed"
-    set +e
-fi

modules.d/01fips/fips-boot.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+if ! fipsmode=$(getarg fips) || [ $fipsmode = "0" ]; then
+    rm -f /etc/modprobe.d/fips.conf >/dev/null 2>&1
+elif getarg boot= >/dev/null; then
+    . /sbin/fips.sh
+    if mount_boot; then
+        do_fips || die "FIPS integrity test failed"
+    fi
+fi

modules.d/01fips/fips-noboot.sh
@@ -1,0 +1,11 @@
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+if ! fipsmode=$(getarg fips) || [ $fipsmode = "0" ]; then
+    rm -f /etc/modprobe.d/fips.conf >/dev/null 2>&1
+elif ! [ -f /tmp/fipsdone ]; then
+    . /sbin/fips.sh
+    mount_boot
+    do_fips || die "FIPS integrity test failed"
+fi

modules.d/01fips/module-setup.sh
@@ -26,7 +26,10 @@ installkernel() {
 }
 
 install() {
-    inst_hook pre-trigger 01 "$moddir/fips.sh"
+    inst_hook pre-trigger 01 "$moddir/fips-boot.sh"
+    inst_hook pre-pivot 01 "$moddir/fips-noboot.sh"
+    inst "$moddir/fips.sh" /sbin/fips.sh
+
     dracut_install sha512hmac rmmod insmod mount uname umount
 
     for dir in "$usrlibdir" "$libdir"; do
@@ -37,5 +40,8 @@ install() {
     done
 
     dracut_install $usrlibdir/hmaccalc/sha512hmac.hmac
+    if command -v prelink >/dev/null; then
+        dracut_install prelink
+    fi
 }
 

tiltingpenguin/openSUSE-dracut

commit b6b2e0edbfab1e09136e01739f0287eb0d1efc97

fix(fips.sh): respect rd.fips.skipkernel
Support for this flag was broken in 4257798f8ad8570e6918ca9b819ffa254d4316ff

(cherry picked from commit 5789abcbe05f30d556086590b786c4857d025d9d)

modules.d/01fips/fips.sh
@@ -170,53 +170,56 @@ do_fips() {
 
     KERNEL=$(uname -r)
 
-    fips_info "Checking integrity of kernel"
-    if [ -e "/run/initramfs/live/vmlinuz0" ]; then
-        do_rhevh_check /run/initramfs/live/vmlinuz0 || return 1
-    elif [ -e "/run/initramfs/live/isolinux/vmlinuz0" ]; then
-        do_rhevh_check /run/initramfs/live/isolinux/vmlinuz0 || return 1
-    elif [ -e "/run/install/repo/images/pxeboot/vmlinuz" ]; then
-        # This is a boot.iso with the .hmac inside the install.img
-        do_rhevh_check /run/install/repo/images/pxeboot/vmlinuz || return 1
-    else
-        BOOT_IMAGE="$(getarg BOOT_IMAGE)"
+    if ! getarg rd.fips.skipkernel > /dev/null; then
 
-        # Trim off any leading GRUB boot device (e.g. ($root) )
-        BOOT_IMAGE="$(echo "${BOOT_IMAGE}" | sed 's/^(.*)//')"
-
-        BOOT_IMAGE_NAME="${BOOT_IMAGE##*/}"
-        BOOT_IMAGE_PATH="${BOOT_IMAGE%${BOOT_IMAGE_NAME}}"
-
-        if [ -z "$BOOT_IMAGE_NAME" ]; then
-            BOOT_IMAGE_NAME="${_vmname}-${KERNEL}"
-        elif ! [ -e "/boot/${BOOT_IMAGE_PATH}/${BOOT_IMAGE}" ]; then
-            #if /boot is not a separate partition BOOT_IMAGE might start with /boot
-            BOOT_IMAGE_PATH=${BOOT_IMAGE_PATH#"/boot"}
-            #on some achitectures BOOT_IMAGE does not contain path to kernel
-            #so if we can't find anything, let's treat it in the same way as if it was empty
-            if ! [ -e "/boot/${BOOT_IMAGE_PATH}/${BOOT_IMAGE_NAME}" ]; then
-                BOOT_IMAGE_NAME="${_vmname}-${KERNEL}"
-                BOOT_IMAGE_PATH=""
-            fi
-        fi
-
-        BOOT_IMAGE_HMAC="/boot/${BOOT_IMAGE_PATH}/.${BOOT_IMAGE_NAME}.hmac"
-        if ! [ -e "${BOOT_IMAGE_HMAC}" ]; then
-            warn "${BOOT_IMAGE_HMAC} does not exist"
-            return 1
-        fi
-
-        BOOT_IMAGE_KERNEL="/boot/${BOOT_IMAGE_PATH}${BOOT_IMAGE_NAME}"
-        if ! [ -e "${BOOT_IMAGE_KERNEL}" ]; then
-            warn "${BOOT_IMAGE_KERNEL} does not exist"
-            return 1
-        fi
-
-        if [ -n "$(fipscheck)" ]; then
-            $(fipscheck) "${BOOT_IMAGE_KERNEL}" || return 1
+        fips_info "Checking integrity of kernel"
+        if [ -e "/run/initramfs/live/vmlinuz0" ]; then
+            do_rhevh_check /run/initramfs/live/vmlinuz0 || return 1
+        elif [ -e "/run/initramfs/live/isolinux/vmlinuz0" ]; then
+            do_rhevh_check /run/initramfs/live/isolinux/vmlinuz0 || return 1
+        elif [ -e "/run/install/repo/images/pxeboot/vmlinuz" ]; then
+            # This is a boot.iso with the .hmac inside the install.img
+            do_rhevh_check /run/install/repo/images/pxeboot/vmlinuz || return 1
         else
-            warn "Could not find fipscheck to verify MACs"
-            return 1
+            BOOT_IMAGE="$(getarg BOOT_IMAGE)"
+
+            # Trim off any leading GRUB boot device (e.g. ($root) )
+            BOOT_IMAGE="$(echo "${BOOT_IMAGE}" | sed 's/^(.*)//')"
+
+            BOOT_IMAGE_NAME="${BOOT_IMAGE##*/}"
+            BOOT_IMAGE_PATH="${BOOT_IMAGE%${BOOT_IMAGE_NAME}}"
+
+            if [ -z "$BOOT_IMAGE_NAME" ]; then
+                BOOT_IMAGE_NAME="${_vmname}-${KERNEL}"
+            elif ! [ -e "/boot/${BOOT_IMAGE_PATH}/${BOOT_IMAGE}" ]; then
+                #if /boot is not a separate partition BOOT_IMAGE might start with /boot
+                BOOT_IMAGE_PATH=${BOOT_IMAGE_PATH#"/boot"}
+                #on some achitectures BOOT_IMAGE does not contain path to kernel
+                #so if we can't find anything, let's treat it in the same way as if it was empty
+                if ! [ -e "/boot/${BOOT_IMAGE_PATH}/${BOOT_IMAGE_NAME}" ]; then
+                    BOOT_IMAGE_NAME="${_vmname}-${KERNEL}"
+                    BOOT_IMAGE_PATH=""
+                fi
+            fi
+
+            BOOT_IMAGE_HMAC="/boot/${BOOT_IMAGE_PATH}/.${BOOT_IMAGE_NAME}.hmac"
+            if ! [ -e "${BOOT_IMAGE_HMAC}" ]; then
+                warn "${BOOT_IMAGE_HMAC} does not exist"
+                return 1
+            fi
+
+            BOOT_IMAGE_KERNEL="/boot/${BOOT_IMAGE_PATH}${BOOT_IMAGE_NAME}"
+            if ! [ -e "${BOOT_IMAGE_KERNEL}" ]; then
+                warn "${BOOT_IMAGE_KERNEL} does not exist"
+                return 1
+            fi
+
+            if [ -n "$(fipscheck)" ]; then
+                $(fipscheck) "${BOOT_IMAGE_KERNEL}" || return 1
+            else
+                warn "Could not find fipscheck to verify MACs"
+                return 1
+            fi
         fi
     fi
 