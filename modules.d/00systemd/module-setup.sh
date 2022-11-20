de09baad4f07a72de41ae15a4cb97f64eed9ac4e
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-11-20 23:58:31 +0000

    chore(shfmt): update to pass with shfmt v3.5.1
    (cherry picked from commit 16931f5236b44078ab2bbc60d7b3bef2ba4507d7)

--- modules.d/00systemd/module-setup.sh
+++ modules.d/00systemd/module-setup.sh
@@ -47,7 +47,6 @@ install() {
         "$systemdutildir"/system-generators/systemd-debug-generator \
         "$systemdutildir"/system-generators/systemd-fstab-generator \
         "$systemdutildir"/system-generators/systemd-gpt-auto-generator \
-        \
         "$systemdsystemunitdir"/debug-shell.service \
         "$systemdsystemunitdir"/cryptsetup.target \
         "$systemdsystemunitdir"/cryptsetup-pre.target \
@@ -79,9 +78,7 @@ install() {
         "$systemdsystemunitdir"/timers.target \
         "$systemdsystemunitdir"/paths.target \
         "$systemdsystemunitdir"/umount.target \
-        \
         "$systemdsystemunitdir"/sys-kernel-config.mount \
-        \
         "$systemdsystemunitdir"/modprobe@.service \
         "$systemdsystemunitdir"/kmod-static-nodes.service \
         "$systemdsystemunitdir"/systemd-tmpfiles-setup.service \
@@ -107,7 +104,6 @@ install() {
         "$systemdsystemunitdir"/systemd-vconsole-setup.service \
         "$systemdsystemunitdir"/systemd-volatile-root.service \
         "$systemdsystemunitdir"/systemd-sysctl.service \
-        \
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-modules-load.service \
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-ask-password-console.path \
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-journald.service \
@@ -122,18 +118,14 @@ install() {
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-tmpfiles-setup.service \
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-tmpfiles-setup-dev.service \
         "$systemdsystemunitdir"/sysinit.target.wants/systemd-sysctl.service \
-        \
         "$systemdsystemunitdir"/ctrl-alt-del.target \
         "$systemdsystemunitdir"/reboot.target \
         "$systemdsystemunitdir"/systemd-reboot.service \
         "$systemdsystemunitdir"/syslog.socket \
-        \
         "$systemdsystemunitdir"/slices.target \
         "$systemdsystemunitdir"/system.slice \
         "$systemdsystemunitdir"/-.slice \
-        \
         "$tmpfilesdir"/systemd.conf \
-        \
         journalctl systemctl \
         echo swapoff \
         kmod insmod rmmod modprobe modinfo depmod lsmod \
